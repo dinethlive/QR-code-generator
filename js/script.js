71517818689d2f60d0a84d5091332d8ba5ac34e3
dinethlive <dinethlive@gmail.com>
2022-08-17 07:35:47 +0530

    add clear ui function to avoid QR code duplication

--- js/script.js
+++ js/script.js
@@ -2,20 +2,34 @@ const form = document.getElementById("generate-form");
 const qr = document.getElementById("qrcode");
 
 const onGenerateSubmit = (e) => {
+  clearUI();
   e.preventDefault();
 
   const url = document.getElementById("url").value;
   const size = document.getElementById("size").value;
 
-  console.log(url, size);
-
   if (url === "") {
     hideSpinner();
   } else {
     showSpinner();
+    setTimeout(() => {
+      hideSpinner();
+      generateQRCode(url, size);
+    }, 1000);
   }
 };
-
+// Generate QR code
+const generateQRCode = (url, size) => {
+  const qrcode = new QRCode("qrcode", {
+    text: url,
+    width: size,
+    height: size,
+  });
+};
+// Clear QR code and save button
+const clearUI = () => {
+  qr.innerHTML = "";
+};
 const showSpinner = () => {
   document.getElementById("spinner").style.display = "block";
 };
